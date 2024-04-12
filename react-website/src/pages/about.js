283601e5f01698843ed3cffec154bfce0a2c5c99
Sunjae Yoo <sun030224@naver.com>
2024-04-12 16:49:16 +0900

    1)

---
+++ react-website/src/pages/about.js
@@ -0,0 +1,17 @@
+
+// Filename - pages/about.js
+ 
+import React from "react";
+ 
+const About = () => {
+    return (
+        <div>
+            <h1>
+                GeeksforGeeks is a Computer Science portal
+                for geeks.
+            </h1>
+        </div>
+    );
+};
+ 
+export default About;
---
+++ react-website/src/pages/contact.js
@@ -0,0 +1,17 @@
+
+// Filename - pages/contact.js
+ 
+import React from "react";
+ 
+const Contact = () => {
+    return (
+        <div>
+            <h1>
+                Mail us on 
+                feedback@geeksforgeeks.org
+            </h1>
+        </div>
+    );
+};
+ 
+export default Contact;
